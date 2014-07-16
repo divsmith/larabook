4ccd78a08369a32d529e6a51d6ffd7bbf0096800
Parker Smith <parker@parkersmith.us>
2014-07-15 22:52:26 -0600

    Add master page

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
+</head>
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
+ 
---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+    Welcome to Larabook.
+@stop
