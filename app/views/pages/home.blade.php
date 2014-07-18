af11cd35fb1953283cb14aff066ab69c4346e03c
Parker Smith <parker@parkersmith.us>
2014-07-17 22:17:55 -0600

    Update signup link to route

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -5,7 +5,7 @@
         <h1>Welcome to Larabook!</h1>
         <p>Welcome to the premier place to talk about Laravel with others. Why don't you sign up to see what all the fuss is about?</p>
         <p>
-            <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign Up!</a>
+            {{ link_to_route('register_path', 'Sign Up!', null, ['class' => 'btn btn-lg btn-primary']) }}
         </p>
     </div>
 @stop
