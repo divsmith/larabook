f2ee7a23abda5f2395ca91352d6bc91783c80998
Parker Smith <parker@parkersmith.us>
2014-07-20 13:30:49 -0500

    Add sign in form view

---
+++ app/views/sessions/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.default')
+
+@section('content')
+    <h1>Sign In!</h1>
+
+    {{ Form::open(['route' => 'login_path']) }}
+        <!-- Email Form Input -->
+        <div class="form-group">
+            {{ Form::label('email', 'Email:') }}
+            {{ Form::email('email', null, ['class' => 'form-control', 'required' => 'required']) }}
+        </div>
+
+        <!-- Password Form Input -->
+        <div class="form-group">
+            {{ Form::label('password', 'Password:') }}
+            {{ Form::password('password', ['class' => 'form-control', 'required' => 'required']) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::submit('Sign In', ['class' => 'btn btn-primary']) }}
+        </div>
+    {{ Form::close() }}
+@stop
