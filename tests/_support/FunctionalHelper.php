bac38e45ba731602b091e79f6d6e9f01bcefb207
Parker Smith <parker@parkersmith.us>
2014-07-20 17:31:29 -0500

    Add test for posting statuses

--- tests/_support/FunctionalHelper.php
+++ tests/_support/FunctionalHelper.php
@@ -22,6 +22,20 @@ class FunctionalHelper extends \Codeception\Module {
         $I->click('Sign In');
     }
 
+    public function postAStatus($body)
+    {
+        $I = $this->getModule('Laravel4');
+
+        $I->fillField('Status:', $body);
+        $I->click('Post Status');
+        // $this->have('Larabook\Statuses\Status', $overrides);
+    }
+
+    public function have($model, $overrides = [])
+    {
+        return TestDummy::create($model, $overrides);
+    }
+
     public function haveAnAccount($overrides = [])
     {
         TestDummy::create('Larabook\Users\User', $overrides);
