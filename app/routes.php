562259e676ca81dc024f7d5de9bcbd66ec5b258a
Parker Smith <parker@parkersmith.us>
2014-07-20 13:31:34 -0500

    Add routes for sessions and basic status page

--- app/routes.php
+++ app/routes.php
@@ -28,3 +28,21 @@ Route::post('register', [
     'as' => 'register_path',
     'uses' => 'RegistrationController@store'
 ]);
+
+/**
+ * Sessions
+ */
+Route::get('login', [
+    'as' => 'login_path',
+    'uses' => 'SessionsController@create'
+]);
+
+Route::post('login', [
+    'as' => 'login_path',
+    'uses' => 'SessionsController@store'
+]);
+
+/**
+ * Statuses
+ */
+Route::get('statuses', 'StatusController@index');
