d27ffc1c066af1a42bd728e2b6533b6690a92778
Parker Smith <parker@parkersmith.us>
2014-07-20 11:23:58 -0600

    Give current user to all views

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -4,8 +4,11 @@
     <div class="jumbotron">
         <h1>Welcome to Larabook!</h1>
         <p>Welcome to the premier place to talk about Laravel with others. Why don't you sign up to see what all the fuss is about?</p>
-        <p>
-            {{ link_to_route('register_path', 'Sign Up!', null, ['class' => 'btn btn-lg btn-primary']) }}
-        </p>
+
+        @if( ! $currentUser )
+            <p>
+                {{ link_to_route('register_path', 'Sign Up!', null, ['class' => 'btn btn-lg btn-primary']) }}
+            </p>
+        @endif
     </div>
 @stop
