09cf144044da119963df6c8d3b8d28713bc6dd57
Parker Smith <parker@parkersmith.us>
2014-07-20 13:29:04 -0500

    Add test for signing a user in

---
+++ tests/functional/SignInCept.php
@@ -0,0 +1,10 @@
+<?php
+
+$I = new FunctionalTester($scenario);
+$I->am('a Larabook member');
+$I->wantTo('login to my Larabook account');
+
+$I->signIn();
+
+$I->seeInCurrentUrl('/statuses');
+$I->see('Welcome back!');
