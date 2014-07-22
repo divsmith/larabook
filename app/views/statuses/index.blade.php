68253bef22a89fc513d87f3242e7f2b84476530d
Parker Smith <parker@parkersmith.us>
2014-07-22 13:36:22 -0500

    Finish status styling

--- app/views/statuses/index.blade.php
+++ app/views/statuses/index.blade.php
@@ -6,31 +6,21 @@
 
             @include('layouts.partials.errors')
 
-            {{ Form::open() }}
-                <!-- Status Form Input -->
-                <div class="form-group">
-                    {{ Form::label('body', 'Status:') }}
-                    {{ Form::textarea('body', null, ['class' => 'form-control']) }}
-                </div>
+            <div class="status-post">
+                {{ Form::open() }}
+                    <!-- Status Form Input -->
+                    <div class="form-group">
+                        {{ Form::textarea('body', null, ['class' => 'form-control', 'rows' => 3, 'placeholder' => "What's on your mind?"]) }}
+                    </div>
 
-                <div class="form-group">
-                    {{ Form::submit('Post Status', ['class' => 'btn btn-primary']) }}
-                </div>
-            {{ Form::close() }}
+                    <div class="form-group status-post-submit">
+                        {{ Form::submit('Post Status', ['class' => 'btn btn-default btn-xs pull-right']) }}
+                    </div>
+                {{ Form::close() }}
+            </div>
 
             @foreach($statuses as $status)
-                <article class="media status-media">
-                    <div class="pull-left">
-                        <img class="media-object" src="{{ $status->user->present()->gravatar }}" alt="{{ $status->user->username }}"/>
-                    </div>
-
-                    <div class="media-body">
-                        <h4 class="media-heading">{{ $status->user->username }}</h4>
-                        <p>{{ $status->created_at->diffForHumans() }}</p>
-
-                        {{ $status->body }}
-                    </div>
-                </article>
+                @include('statuses.partials.status')
             @endforeach
         </div>
     </div>
