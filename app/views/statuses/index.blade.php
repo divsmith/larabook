83e2a92610beee3e3634bebb0bebe8aba4eaba15
Parker Smith <parker@parkersmith.us>
2014-07-22 13:09:54 -0500

    Basic status styling

--- app/views/statuses/index.blade.php
+++ app/views/statuses/index.blade.php
@@ -1,27 +1,37 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Post a Status</h1>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
 
-    @include('layouts.partials.errors')
+            @include('layouts.partials.errors')
 
-    {{ Form::open() }}
-        <!-- Status Form Input -->
-        <div class="form-group">
-            {{ Form::label('body', 'Status:') }}
-            {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+            {{ Form::open() }}
+                <!-- Status Form Input -->
+                <div class="form-group">
+                    {{ Form::label('body', 'Status:') }}
+                    {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+                </div>
+
+                <div class="form-group">
+                    {{ Form::submit('Post Status', ['class' => 'btn btn-primary']) }}
+                </div>
+            {{ Form::close() }}
+
+            @foreach($statuses as $status)
+                <article class="media status-media">
+                    <div class="pull-left">
+                        <img class="media-object" src="{{ $status->user->present()->gravatar }}" alt="{{ $status->user->username }}"/>
+                    </div>
+
+                    <div class="media-body">
+                        <h4 class="media-heading">{{ $status->user->username }}</h4>
+                        <p>{{ $status->created_at->diffForHumans() }}</p>
+
+                        {{ $status->body }}
+                    </div>
+                </article>
+            @endforeach
         </div>
-
-        <div class="form-group">
-            {{ Form::submit('Post Status', ['class' => 'btn btn-primary']) }}
-        </div>
-    {{ Form::close() }}
-
-    <h2>Statuses</h2>
-
-    @foreach($statuses as $status)
-        <article>
-            {{ $status->body }}
-        </article>
-    @endforeach
+    </div>
 @stop
