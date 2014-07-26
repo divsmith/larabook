62b9b44d65c6edbf54c63288a094b44a019b7616
Parker Smith <parker@parkersmith.us>
2014-07-25 23:22:56 -0500

    Add, style user index page

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -3,10 +3,17 @@
 @section('content')
     <h1>All Users</h1>
 
-    @foreach( $users as $user )
-        <div class="col-md-3">
-            @include('layouts.partials.avatar')
-            {{ $user->username }}
+    @foreach( $users->chunk(4) as $userSet )
+        <div class="row users">
+            @foreach( $userSet as $user )
+                <div class="col-md-3 user-block">
+                    @include('layouts.partials.avatar', ['size' => 70] )
+
+                    <h4 class="user-block-username">{{ $user->username }}</h4>
+                </div>
+            @endforeach
         </div>
     @endforeach
+
+    {{ $users->links() }}
 @stop
