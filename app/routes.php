0ae646e492419aea96fd2aa99e999d2256efd8f8
Parker Smith <parker@parkersmith.us>
2014-08-20 00:13:07 +0000

    Add follow user routes

--- app/routes.php
+++ app/routes.php
@@ -72,3 +72,16 @@ Route::get('@{username}', [
     'as' => 'profile_path',
     'uses' => 'UsersController@show'
 ]);
+
+/**
+ * Follows
+ */
+Route::post('follows', [
+    'as' => 'follows_path',
+    'uses' => 'FollowsController@store'
+]);
+
+Route::delete('follows/{id}', [
+    'as' => 'follow_path',
+    'uses' => 'FollowsController@delete'
+]);
