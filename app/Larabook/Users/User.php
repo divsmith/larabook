84ddfb47860c39d5540db01221827e2501952071
Parker Smith <parker@parkersmith.us>
2014-08-26 04:21:56 +0000

    Refactor status method

--- app/Larabook/Users/User.php
+++ app/Larabook/Users/User.php
@@ -58,7 +58,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
      */
     public function statuses()
     {
-        return $this->hasMany('Larabook\Statuses\Status');
+        return $this->hasMany('Larabook\Statuses\Status')->latest();
     }
     
     /**
