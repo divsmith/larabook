b707de6d5fed2b2668bf85e76eda3709de863687
Parker Smith <parker@parkersmith.us>
2014-08-27 02:38:43 +0000

    Create email notifier

---
+++ app/Larabook/Handlers/EmailNotifier.php
@@ -0,0 +1,27 @@
+<?php namespace Larabook\Handlers; 
+
+use Larabook\Mailers\UserMailer;
+use Larabook\Registration\Events\UserHasRegistered;
+use Laracasts\Commander\Events\EventListener;
+
+class EmailNotifier extends EventListener {
+
+    /**
+     * @var UserMailer
+     */
+    private $mailer;
+
+    /**
+     * @param UserMailer $mailer
+     */
+    public function __construct(UserMailer $mailer){
+
+        $this->mailer = $mailer;
+    }
+
+    public function whenUserHasRegistered(UserHasRegistered $event)
+    {
+        $this->mailer->sendWelcomeMessageTo($event->user);
+    }
+    
+} 
