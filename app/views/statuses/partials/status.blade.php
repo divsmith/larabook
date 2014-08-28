f3008493f17727211a6d9693cb0e4e23a5fe9149
Parker Smith <parker@parkersmith.us>
2014-08-28 05:30:17 +0000

    Update to reflect new style

--- app/views/statuses/partials/status.blade.php
+++ app/views/statuses/partials/status.blade.php
@@ -1,12 +1,11 @@
 <article class="media status-media">
-    <div class="pull-left">
-        @include('users.partials.avatar', ['user' => $status->user])
-    </div>
-
-    <div class="media-body">
-        <h4 class="media-heading">{{ $status->user->username }}</h4>
-        <p>{{ $status->present()->timeSincePublished() }}</p>
-
-        {{ $status->body }}
-    </div>
+  <div class="pull-left">
+      <img class="media-object status-media-object" src="{{ $status->user->present()->gravatar(55) }}" alt="{{ $status->user->username }}">
+  </div>
+  <div class="media-body status-media-body">
+      <h4 class="media-heading status-media-heading">{{ $status->user->username }}</h4>
+ 
+      <p><small class="status-media-time">{{ $status->present()->timeSincePublished() }}</small></p>
+      {{ $status->body }}
+  </div>
 </article>
