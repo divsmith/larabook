a92a2ea8fc4fa047e811b37b6f16fe0a7cf0f51e
Parker Smith <parker@parkersmith.us>
2014-08-29 04:09:10 +0000

    Add password reset link

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -21,6 +21,7 @@
 
             <div class="form-group">
                 {{ Form::submit('Sign In', ['class' => 'btn btn-primary']) }}
+                {{ link_to('/password/remind', 'Forgot your password?') }}
             </div>
         {{ Form::close() }}
 
